c7934e9f0fe0e00588bb136df7a6f14cca19e04f
ccopenh1 <ccopenh1@uncc.edu>
2024-11-20 21:24:12 -0500

    pass song_key to detected.html

--- app.py
+++ app.py
@@ -173,6 +173,8 @@ def from_history():
         song_data = json.loads(song_data_json)
         print(f"Song data: {song_data}")
 
+        print(f"###################  S   E   S   S  I  O  N   ###################", {session})
+
 
         # Pass the retrieved data to the template
         return render_template(
